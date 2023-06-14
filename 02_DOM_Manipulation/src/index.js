b679bd7acb41401441b3a2d3baeffd0fe7afb560
betalantz <betalantz@gmail.com>
2023-06-13 17:18:57 -0700

    clean up code and comments

--- 02_DOM_Manipulation/src/index.js
+++ 02_DOM_Manipulation/src/index.js
@@ -21,48 +21,8 @@ function renderFooter(){
 
 }
 
-const bookList = document.querySelector('#book-list')
-bookList.innerHTML = ""
-
-
-function renderBook(bookObj){
-  const li = document.createElement('li')
-  const h3 = document.createElement('h3')
-  const pAuthor = document.createElement('p')
-  const pPrice = document.createElement('p')
-  const img = document.createElement('img')
-  const btn = document.createElement('button')
-  
-  h3.textContent = bookObj.title
-  pAuthor.textContent = bookObj.author
-  pPrice.textContent = formatPrice(bookObj.price)
-  btn.textContent = "Delete"
-  
-  img.src = bookObj.imageUrl
-  // li.className = "list-li"
-  li.classList.add('list-li')
-  
-  li.append(h3, pAuthor, pPrice, img, btn)
-  bookList.appendChild(li)
-  // li.innerHTML = `
-  //   <h3>${bookObj.title}</h3>
-  //   <p>${bookObj.author}</p>
-  //   <p>${formatPrice(bookObj.price)}</p>
-  //   <img src=${bookObj.imageUrl} alt="${bookObj.title} cover"/>
-  //   <button>Delete</button>
-  // `
-  // li.classList.add('list-li')
-  // bookList.appendChild(li)
-}
-
-
-/* <li class="list-li">
-<h3>Eloquent JavaScript : A Modern Introduction to Programming</h3>
-<p>Marjin Haverbeke</p>
-<p>$10.00</p>
-<img src="https://images-na.ssl-images-amazon.com/images/I/51IKycqTPUL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg" alt="Eloquent JavaScript cover"/>
-<button>Delete</button>
-</li> */
+const bookList = document.querySelector('#book-list') // selecting "anchor" element in global scope for easy reuse; "anchor" because we will append to it
+bookList.innerHTML = "" // this "erases" the existing <li></li> from created in the DOM from the HTML doc
 
 // create a function called renderBook(book)
 // it will take a book object as an argument
@@ -79,6 +39,35 @@ function renderBook(bookObj){
   //   <button>Delete</button>
   // </li>
   
+  function renderBook(bookObj){
+    const li = document.createElement('li')
+    const h3 = document.createElement('h3')
+    const pAuthor = document.createElement('p')
+    const pPrice = document.createElement('p')
+    const img = document.createElement('img')
+    const btn = document.createElement('button')
+    
+    h3.textContent = bookObj.title
+    pAuthor.textContent = bookObj.author
+    pPrice.textContent = formatPrice(bookObj.price)
+    btn.textContent = "Delete"
+    
+    img.src = bookObj.imageUrl
+    // li.className = "list-li"
+    li.classList.add('list-li')
+    
+    li.append(h3, pAuthor, pPrice, img, btn)
+    bookList.appendChild(li)
+    // li.innerHTML = `  // using innerHTML is shorter, but comes with pitfalls and potential security issues
+    //   <h3>${bookObj.title}</h3>
+    //   <p>${bookObj.author}</p>
+    //   <p>${formatPrice(bookObj.price)}</p>
+    //   <img src=${bookObj.imageUrl} alt="${bookObj.title} cover"/>
+    //   <button>Delete</button>
+    // `
+    // li.classList.add('list-li')
+    // bookList.appendChild(li)
+  }
   
   // Organize function calls
   renderHeader()
