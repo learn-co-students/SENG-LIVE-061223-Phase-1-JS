a9485086b49b442b57ab856409cfba7bdcb8d5bd
betalantz <betalantz@gmail.com>
2023-06-14 17:14:36 -0700

    clean formatting and comments

--- 03_DOM_Events/src/index.js
+++ 03_DOM_Events/src/index.js
@@ -1,33 +1,35 @@
 //document.addEventListener("DOMContentLoaded", () => {// your logic here})
+// this is the way it was done before <script defer />
 
 //////////////////////////////////////
 // Select elements (that will be referenced frequently)
 //////////////////////////////////////
-const toggleFormButton = document.querySelector('#toggleForm')
-const bookForm = document.querySelector("#book-form")
+const toggleFormButton = document.querySelector("#toggleForm");
+const bookForm = document.querySelector("#book-form");
 
 //////////////////////////////////////
 // Helper functions
 //////////////////////////////////////
 function formatPrice(price) {
-  return '$' + Number.parseFloat(price).toFixed(2);
+  return "$" + Number.parseFloat(price).toFixed(2);
 }
 
-function fillIn(form, data){
-    form.title.value = data.title
-    form.author.value = data.author
-    form.price.value = data.price
-    form.imageUrl.value = data.imageUrl
-    form.inventory.value = data.inventory
+function fillIn(form, data) {
+  form.title.value = data.title;
+  form.author.value = data.author;
+  form.price.value = data.price;
+  form.imageUrl.value = data.imageUrl;
+  form.inventory.value = data.inventory;
 }
 
 fillIn(bookForm, {
-  title: 'Designing Data-intensive Applications',
+  title: "Designing Data-intensive Applications",
   author: "Martin Kleppmann",
   price: 22,
   inventory: 1,
-  imageUrl: 'https://m.media-amazon.com/images/I/51ZSpMl1-LL._SX379_BO1,204,203,200_.jpg'
-})
+  imageUrl:
+    "https://m.media-amazon.com/images/I/51ZSpMl1-LL._SX379_BO1,204,203,200_.jpg",
+});
 
 //////////////////////////////////////
 // render functions  (Data => Display)
@@ -35,13 +37,18 @@ fillIn(bookForm, {
 
 // create a function renderHeader() that takes the store name from bookStore and adds to the DOM
 function renderHeader(bookStore) {
-  document.querySelector('#store-name').textContent = bookStore.name;
+  document.querySelector("#store-name").textContent = bookStore.name;
 }
 
 function renderFooter(bookStore) {
-  document.querySelector('#store').textContent = bookStore.location;
-  document.querySelector('#number').textContent = bookStore.number;
-  document.querySelector('#address').textContent = bookStore.address;
+  document.querySelector("#store").textContent = bookStore.location;
+  document.querySelector("#number").textContent = bookStore.number;
+  document.querySelector("#address").textContent = bookStore.address;
+}
+
+function renderAllBooks(bookStore) {
+  // bookStore.inventory.forEach(book => renderBook(book));
+  bookStore.inventory.forEach(renderBook); // this syntax is shorthand for line above
 }
 
 // function: renderBook(book)
@@ -56,66 +63,65 @@ function renderFooter(bookStore) {
 // </li>
 // appends the li to the ul#book-list in the DOM
 function renderBook(book) {
-    
-  const li = document.createElement('li');
-  li.className = 'list-li';
-  
-  const h3 = document.createElement('h3');
+  const li = document.createElement("li");
+  li.className = "list-li";
+
+  const h3 = document.createElement("h3");
   h3.textContent = book.title;
   li.append(h3);
 
-  const pAuthor = document.createElement('p');
+  const pAuthor = document.createElement("p");
   pAuthor.textContent = book.author;
   li.append(pAuthor);
-  
-  const pPrice = document.createElement('p');
+
+  const pPrice = document.createElement("p");
   pPrice.textContent = formatPrice(book.price);
   li.append(pPrice);
-  
-  const img = document.createElement('img');
+
+  const img = document.createElement("img");
   img.src = book.imageUrl;
   img.alt = `${book.title} cover`;
   img.title = `${book.title} cover`;
   li.append(img);
 
-  const btn = document.createElement('button');
-  btn.textContent = 'Delete';
-  btn.addEventListener('click', () => li.remove())
+  const btn = document.createElement("button");
+  btn.textContent = "Delete";
+  btn.addEventListener("click", () => li.remove());
   li.append(btn);
 
-  document.querySelector('#book-list').append(li);
+  document.querySelector("#book-list").append(li);
 }
 ////////////////////////////////////////////
 // Event listeners & handlers (Behavior -> (Data) -> Display)
 ////////////////////////////////////////////
 
 function toggleBookForm() {
-  const isHidden = bookForm.classList.toggle('collapsed')
+  const isHidden = bookForm.classList.toggle("collapsed"); // .toggle() both add/removes 'collapsed' from classList AND returns a boolean on whether the string will be in the list after execution
   if (isHidden) {
-    toggleFormButton.textContent = "New Book"
+    toggleFormButton.textContent = "New Book";
   } else {
-    toggleFormButton.textContent = "Hide Book Form"
+    toggleFormButton.textContent = "Hide Book Form";
   }
 }
 
-toggleFormButton.addEventListener('click', toggleBookForm)
+toggleFormButton.addEventListener("click", toggleBookForm);
 
-function handleSubmit(event){
+function handleSubmit(event) {
   event.preventDefault();
-  console.dir(event.target)
-  console.log(event.target.title)
-  console.log(event.target.title.value)
+  console.dir(event.target);
+  console.log(event.target.title);
+  console.log(event.target.title.value);
   const newBook = {
-    title: bookForm.title.value,
+    title: bookForm.title.value, // since the event.target IS the form, one could substitue bookForm here, but extracting the value from event.target is safer due to closure
     author: event.target.author.value,
     price: event.target.price.value,
     reviews: [],
     inventory: event.target.inventory.value,
-    imageUrl: event.target.imageUrl.value
-  }
-  renderBook(newBook) // bypassing data, going straight to display
-  event.target.reset() // nice to have for user
-  toggleBookForm() // nice to have to UI
+    imageUrl: event.target.imageUrl.value,
+  };
+  renderBook(newBook); // bypassing data, going straight to display
+  event.target.reset(); // nice to have for user
+  toggleBookForm(); // nice to have to UI
 }
 
 // sample book object
@@ -129,15 +135,16 @@ function handleSubmit(event){
 //   imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/51IKycqTPUL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
 // }
 
-bookForm.addEventListener('submit', handleSubmit)
+bookForm.addEventListener("submit", handleSubmit);
 
-document.addEventListener('keydown', (e) => {
-  console.log(e.key)
-  const isVisible = !bookForm.classList.contains('collapsed')
+document.addEventListener("keydown", (e) => {
+  // could also listen on window
+  console.log(e.key);
+  const isVisible = !bookForm.classList.contains("collapsed");
   if (isVisible && e.code == "Escape") {
-    toggleBookForm()
+    toggleBookForm();
   }
-})
+});
 
 ////////////////////////////////////////////
 // call render functions to populate the DOM
@@ -145,5 +152,5 @@ document.addEventListener('keydown', (e) => {
 
 renderHeader(bookStore);
 renderFooter(bookStore);
-bookStore.inventory.forEach(renderBook);
-
+// bookStore.inventory.forEach(renderBook);
+renderAllBooks(bookStore);
